e1d051ed23d2d9c810eb00f3920f9f727ae467b7
Abhiram Pai <abhirampai1999@gmail.com>
2023-12-06 19:15:28 +0530

    Mention that time used for filtering the options is current browser time

--- src/components/Bus/List.jsx
+++ src/components/Bus/List.jsx
@@ -1,9 +1,23 @@
 import Show from "./Show";
 
 const List = ({ buses, filteredSchedule }) => {
+  const timeOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  };
   return (
     <div className="p-5">
-      <div className="text-lg text-semibold text-center">Found {buses.length} buses</div>
+      <div className="text-lg text-semibold text-center">
+        Found {buses.length} buses
+      </div>
+      <div className="text-center">
+        All results filtered based on current time:{" "}
+        {new Date().toLocaleDateString(undefined, timeOptions)}
+      </div>
       {buses.map((bus, idx) => (
         <Show
           key={idx}
