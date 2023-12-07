ba84c5056db249fe3065daf84b5b0c8fc8b4b207
Abhiram Pai <abhirampai1999@gmail.com>
2023-12-07 11:46:16 +0530

    Misc fixes

--- src/components/Bus/Accordian.jsx
+++ src/components/Bus/Accordian.jsx
@@ -5,8 +5,9 @@ import classNames from "classnames";
 const Accordian = ({ header, body }) => {
   const expandClass = useSignal(false);
   const accordianRef = useRef(null);
+  const isAccordianOpen = expandClass.value;
   const onClickAccordian = () => {
-    expandClass.value = expandClass.value ? false : true;
+    expandClass.value = isAccordianOpen ? false : true;
   };
 
   return (
@@ -15,10 +16,10 @@ const Accordian = ({ header, body }) => {
         className={classNames(
           "group flex flex-col gap-2 rounded-lg bg-black p-5 text-white dark:bg-gray-200 dark:text-gray-400 cursor-pointer",
           {
-            "overflow-scroll": expandClass.value,
+            "overflow-scroll": isAccordianOpen,
           },
           {
-            "overflow-hidden": !expandClass.value,
+            "overflow-hidden": !isAccordianOpen,
           }
         )}
         tabIndex="1"
@@ -30,9 +31,12 @@ const Accordian = ({ header, body }) => {
           <img
             alt="expand or collapse button"
             src="https://upload.wikimedia.org/wikipedia/commons/9/96/Chevron-icon-drop-down-menu-WHITE.png"
-            className={classNames("h-2 w-3 transition-all duration-500 expand-collapse-icon", {
-              "-rotate-180": expandClass.value,
-            })}
+            className={classNames(
+              "h-2 w-3 transition-all duration-500 expand-collapse-icon",
+              {
+                "-rotate-180": isAccordianOpen,
+              }
+            )}
           />
         </div>
         <div
@@ -40,10 +44,10 @@ const Accordian = ({ header, body }) => {
             "h-auto max-h-0 items-center opacity-0 transition-all",
             {
               "visible max-h-full opacity-100 duration-1000 border-t border-t-zinc-200":
-                expandClass.value,
+                isAccordianOpen,
             },
             {
-              "invisible -z-10 h-0": !expandClass.value,
+              "invisible -z-10 h-0": !isAccordianOpen,
             }
           )}
         >
