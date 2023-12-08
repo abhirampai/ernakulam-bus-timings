281a64ef0e8278564adfd73ab329ded28969c0b2
Abhiram Pai <abhirampai1999@gmail.com>
2023-12-08 15:56:54 +0530

    Added translation for app and bus timings file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import BusTimings from "./components/BusTimings";
+import i18next from "./i18n";
 
 const queryClient = new QueryClient();
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="text-3xl font-bold p-5 text-center dark:text-gray-100">
-        Ernakulam Private Bus Timings
+        {i18next.t("landingPageHeading")}
       </div>
       <BusTimings />
       <ReactQueryDevtools initialIsOpen={false} />
