45e32dc6039248fa4911ecdcf052d5169ed9abfd
Abhiram Pai <abhirampai1999@gmail.com>
2023-12-12 15:00:41 +0530

    Fix show after accordion and show route fix

--- src/components/Bus/Show.jsx
+++ src/components/Bus/Show.jsx
@@ -1,8 +1,9 @@
 import { useMemo } from "react";
-import { Accordian, Modal, Progress, Table } from "../common";
+import { Accordion, Modal, Progress, Table } from "../common";
 import { useLocalizedTranslation } from "hooks/utils";
 import moment from "moment";
 import { useSignal } from "@preact/signals-react";
+import ShowRoute from "./ShowRoute";
 
 const Show = ({ number, routes, schedules, currentTrip }) => {
   const openModal = useSignal(false);
@@ -95,64 +96,18 @@ const Show = ({ number, routes, schedules, currentTrip }) => {
     </>
   );
 
-  const showRoute = () => (
-    <div className="absolute z-50 w-1/4 overflow-hidden text-left bg-black dark:bg-white border rounded-sm">
-      <div className="my-5 overflow-y-auto"></div>
-      <div className="flex flex-col w-full px-8 pb-6 space-y-6">
-        <div className="flex w-full justify-between">
-          <div className="text-white dark:text-gray-700 capitalize">
-            {number}
-          </div>
-          <button
-            onClick={() => {
-              openModal.value = false;
-              document.body.removeAttribute("style");
-            }}
-            className="text-gray-500 hover:text-gray-700"
-          >
-            <span className="sr-only">{t("common.close")}</span>
-            <svg
-              className="h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              ></path>
-            </svg>
-          </button>
-        </div>
-        <div className="flex gap-2 mt-2">
-          <label className="text-white dark:text-gray-700 capitalize">
-            {t("bus.route")}:{" "}
-          </label>
-          <Progress
-            items={routes.map((route) => ({
-              name: route.toLowerCase(),
-            }))}
-          />
-        </div>
-      </div>
-    </div>
-  );
-
   return (
     <>
-      <Accordian header={header()} body={body()} />
+      <Accordion header={header()} body={body()} />
       <Modal
         open={openModal.value}
         onCloseHandler={() => {
           openModal.value = false;
           document.body.style.overflow = "auto";
         }}
-        children={showRoute()}
-      />
+      >
+        <ShowRoute number={number} openModal={openModal} routes={routes} />
+      </Modal>
     </>
   );
 };
